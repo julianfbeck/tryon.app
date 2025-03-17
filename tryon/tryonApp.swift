92b4ae8b414c8ceec3c6bd47f1521cfaf6d173bc
Julian Beck <ju-fa-beck@t-online.de>
2025-03-17 19:14:53 +0100

    Initial Commit

---
+++ tryon/tryonApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryonApp.swift
+//  tryon
+//
+//  Created by Julian Beck on 17.03.25.
+//
+
+import SwiftUI
+
+@main
+struct tryonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
